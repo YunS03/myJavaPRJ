c5b5791de059533a3f1f1663bdd1d657385d77b8
YunS03 <104403338+YunS03@users.noreply.github.com>
2022-04-27 09:20:35 +0900

    Add files via upload

---
+++ Ex03_04.java
@@ -0,0 +1,12 @@
+public class Ex03_04 {
+    public static void main(String[] args) {
+        String str1 = "IT CookBook 입니다.";
+        String str2 = "10";
+        String str3 = "20";
+
+        str1 = "Java 입니다. ";
+
+        System.out.printf("%s Wn", str1);
+        System.out.printf("%s Wn", str2+str3);
+    }
+}
